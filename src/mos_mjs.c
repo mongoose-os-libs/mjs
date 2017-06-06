21d150c8eaf9c65e35bf48906b1aaa8893c0fe2b
Dmitry Frank <mail@dmitryfrank.com>
2017-06-06 12:56:17 +0100

    Add mjs.generate_jsc config value
    PUBLISHED_FROM=a4ca585215eb562e89b55abb44e575ccfb06111c

--- src/mos_mjs.c
+++ src/mos_mjs.c
@@ -8,12 +8,14 @@
 
 bool mgos_mjs_init(void) {
   /* Initialize JavaScript engine */
+  struct sys_config *cfg = get_cfg();
   int mem1, mem2, mem3;
   mem1 = mgos_get_free_heap_size();
   struct mjs *mjs = mjs_create();
   mem2 = mgos_get_free_heap_size();
   mjs_set_ffi_resolver(mjs, mgos_dlsym);
-  mjs_err_t err = mjs_exec_file(mjs, "init.js", 1, NULL);
+  mjs_set_generate_jsc(mjs, cfg->mjs.generate_jsc);
+  mjs_err_t err = mjs_exec_file(mjs, "init.js", NULL);
   if (err != MJS_OK) {
     mjs_print_error(mjs, stdout, NULL, 1 /* print_stack_trace */);
   }
