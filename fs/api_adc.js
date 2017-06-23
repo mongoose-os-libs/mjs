b398764a2904c1ee96ce0c9b1283dd0695965ca1
ruslanvaliullin <r.valiullin77@gmail.com>
2017-06-23 23:55:50 +0500

    Implement ADC support for esp32
    PUBLISHED_FROM=9661acf93a76852de62628c63ea67da38cde3aae

--- fs/api_adc.js
+++ fs/api_adc.js
@@ -1,4 +1,8 @@
 let ADC = {
+  // ## **`ADC.enable(pin)`**
+  // Configure and enable ADC for a `pin`,
+  // return 1 if success, 0 otherwise.
+  enable: ffi('int mgos_adc_enable(int)'),
   // ## **`ADC.read(pin)`**
   // Read `pin` analog value, return an integer.
   read: ffi('int mgos_adc_read(int)'),
