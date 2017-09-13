d9802b748b44505f8eb9b6f02664dbca883802cf
Deomid Ryabkov <rojer@cesanta.com>
2017-09-13 18:25:15 +0300

    Add Wifi.scan and example-wifi-scan-js app
    PUBLISHED_FROM=954d2833b7e654de26f8b76a0f55c869d607a283

--- src/mos_mjs.c
+++ src/mos_mjs.c
@@ -1,11 +1,21 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ */
+
 #include "common/platform.h"
+
 #include "mgos_app.h"
+#include "mgos_dlsym.h"
 #include "mgos_hal.h"
 #include "mgos_hooks.h"
 #include "mgos_mongoose.h"
 #include "mgos_net.h"
 #include "mgos_sys_config.h"
-#include "mgos_dlsym.h"
+#ifdef MGOS_HAVE_WIFI
+#include "mgos_wifi.h"
+#endif
+
 #include "mos_mjs.h"
 
 struct mjs *mjs = NULL;
@@ -62,6 +72,15 @@ bool mgos_mjs_init(void) {
   mem2 = mgos_get_free_heap_size();
   mjs_set_ffi_resolver(mjs, mgos_dlsym);
   mjs_set_generate_jsc(mjs, cfg->mjs.generate_jsc);
+
+#ifdef MGOS_HAVE_WIFI
+  {
+    mjs_val_t global_obj = mjs_get_global(mjs);
+    mjs_val_t wifi_obj = mjs_mk_object(mjs);
+    mjs_set(mjs, global_obj, "Wifi", ~0, wifi_obj);
+    mjs_set(mjs, wifi_obj, "scan", ~0, mjs_mk_foreign(mjs, mgos_wifi_scan_js));
+  }
+#endif
 
   /*
    * We also need to run init.js, but we can't do that here because init.js
