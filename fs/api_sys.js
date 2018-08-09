e03e03424fc246de2ae487a50fa65e2c32b1b844
juanolete <31393568+juanolete@users.noreply.github.com>
2018-08-09 17:07:28 -0400

    Update api_sys.js

--- fs/api_sys.js
+++ fs/api_sys.js
@@ -57,5 +57,5 @@ let Sys = {
   // ##**`Sys.wdt_disable()`**
   // Disable watchdog.
   // Return value: none.
-  wdt_disable: ffi('void mgos_wdt_disable(void)´);
+  wdt_disable: ffi('void mgos_wdt_disable(void)');
 };
